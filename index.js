803ad877616a5ee63a3851747745c8ab8a289843
Jonas Brabant <brabantjonas@hotmail.com>
2025-05-07 15:21:17 +0200

    change m to member - proper naming

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const members = require('./team/members.json');
 console.log("Teamleden:");
-members.forEach(m => console.log(`- ${m.name}`));
+members.forEach(member => console.log(`- ${member.name}`));
